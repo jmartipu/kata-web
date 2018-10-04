77e4f815c75326252dc481c8a90076995e2bff2f
Juan Jose Villegas <jj.villegas47@gmail.com>
2018-10-03 20:40:57 -0500

    Updated .gitignore and changed db settings

--- KataWeb/KataWeb/settings.py
+++ KataWeb/KataWeb/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'KataWeb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'kata_web',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
